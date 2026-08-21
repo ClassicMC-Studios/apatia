04011deb9eae39af5cea958e070cc1bee3a90d43
Weston <classicmclive@gmail.com>
2026-08-21 00:01:10 -0400

    Add multiple language exercises to bppt-dezmil.js

--- bppt-dezmil.js
+++ bppt-dezmil.js
@@ -108,6 +108,195 @@ let language = JSON.parse(`
             "b": ["entender", false],
             "c": ["explicar", false],
             "d": ["evitar", false]
+        },
+
+        {
+            "g": "A polícia ___ os bandidos.",
+            "e": "The police arrested the criminals.",
+            "a": ["prendeu", true],
+            "b": ["perseguiu", false],
+            "c": ["encontrou", false],
+            "d": ["interrogou", false]
+        },
+
+        {
+            "g": "A ___ do sistema foi comprometida.",
+            "e": "The security of the system was compromised.",
+            "a": ["segurança", true],
+            "b": ["proteção", false],
+            "c": ["estrutura", false],
+            "d": ["privacidade", false]
+        },
+
+        {
+            "g": "Ele era ___ do roubo.",
+            "e": "He was suspected of the robbery.",
+            "a": ["suspeito", true],
+            "b": ["culpado", false],
+            "c": ["acusado", false],
+            "d": ["testemunha", false]
+        },
+
+        {
+            "g": "A área ___ estava muito silenciosa.",
+            "e": "The surrounding area was very quiet.",
+            "a": ["circundante", true],
+            "b": ["próxima", false],
+            "c": ["vizinha", false],
+            "d": ["distante", false]
+        },
+
+        {
+            "g": "A igreja é ___ por bosques e lagos.",
+            "e": "The church is surrounded by woods and lakes.",
+            "a": ["cercada", true],
+            "b": ["rodeada", false],
+            "c": ["protegida", false],
+            "d": ["coberta", false]
+        },
+
+        {
+            "g": "As ___ em Roma são de tirar o fôlego.",
+            "e": "The views in Rome are breathtaking.",
+            "a": ["vistas", true],
+            "b": ["paisagens", false],
+            "c": ["imagens", false],
+            "d": ["ruínas", false]
+        },
+
+        {
+            "g": "Eles moram no ___.",
+            "e": "They live in the suburbs.",
+            "a": ["subúrbio", true],
+            "b": ["centro", false],
+            "c": ["interior", false],
+            "d": ["bairro", false]
+        },
+
+        {
+            "g": "Nós alugamos uma cabana ___ nas montanhas.",
+            "e": "We rented a cozy cabin in the mountains.",
+            "a": ["aconchegante", true],
+            "b": ["confortável", false],
+            "c": ["pequena", false],
+            "d": ["isolada", false]
+        },
+
+        {
+            "g": "Eu te ___ dos teus pecados.",
+            "e": "I absolve you of your sins.",
+            "a": ["absolvo", true],
+            "b": ["perdoo", false],
+            "c": ["liberto", false],
+            "d": ["salvo", false]
+        },
+
+        {
+            "g": "___ seja o Senhor!",
+            "e": "Praise the Lord!",
+            "a": ["Louvado", true],
+            "b": ["Bendito", false],
+            "c": ["Adorado", false],
+            "d": ["Glorificado", false]
+        },
+
+        {
+            "g": "Nada é ___.",
+            "e": "Nothing is sacred.",
+            "a": ["sagrado", true],
+            "b": ["divino", false],
+            "c": ["eterno", false],
+            "d": ["puro", false]
+        },
+
+        {
+            "g": "Nesse país, as pessoas ___ o dinheiro como um deus.",
+            "e": "In that country, people worshipped money like a god.",
+            "a": ["cultuavam", true],
+            "b": ["adoravam", false],
+            "c": ["respeitavam", false],
+            "d": ["valorizavam", false]
+        },
+
+        {
+            "g": "Ouvimos os ___ da igreja.",
+            "e": "We heard the church bells.",
+            "a": ["sinos", true],
+            "b": ["hinos", false],
+            "c": ["órgãos", false],
+            "d": ["cânticos", false]
+        },
+
+        {
+            "g": "Dê um ___ no Tom.",
+            "e": "Give Tom a kiss.",
+            "a": ["beijo", true],
+            "b": ["abraço", false],
+            "c": ["sorriso", false],
+            "d": ["aceno", false]
+        },
+
+        {
+            "g": "Ela ___ a confiança dele.",
+            "e": "She betrayed his trust.",
+            "a": ["traiu", true],
+            "b": ["perdeu", false],
+            "c": ["quebrou", false],
+            "d": ["ignorou", false]
+        },
+
+        {
+            "g": "Ele me ___ como uma criança.",
+            "e": "He treats me like a child.",
+            "a": ["trata", true],
+            "b": ["vê", false],
+            "c": ["considera", false],
+            "d": ["conhece", false]
+        },
+
+        {
+            "g": "Ele tem muitos ___.",
+            "e": "He has many acquaintances.",
+            "a": ["conhecidos", true],
+            "b": ["amigos", false],
+            "c": ["colegas", false],
+            "d": ["vizinhos", false]
+        },
+
+        {
+            "g": "Eu contei o ___ para o Tom.",
+            "e": "I told Tom the secret.",
+            "a": ["segredo", true],
+            "b": ["problema", false],
+            "c": ["motivo", false],
+            "d": ["aviso", false]
+        },
+
+        {
+            "g": "Eu ___ o seu apoio.",
+            "e": "I value your support.",
+            "a": ["valorizo", true],
+            "b": ["aceito", false],
+            "c": ["agradeço", false],
+            "d": ["reconheço", false]
+        },
+
+        {
+            "g": "Tom tem ___ a Mary.",
+            "e": "Tom has been avoiding Mary.",
+            "a": ["evitado", true],
+            "b": ["ignorado", false],
+            "c": ["seguido", false],
+            "d": ["procurado", false]
+        },
+
+        {
+            "g": "Você logo deixará de ___ nela.",
+            "e": "You will soon stop thinking about her.",
+            "a": ["pensar", true],
+            "b": ["falar", false],
+            "c": ["lembrar", false],
+            "d": ["sonhar", false]
         }
     ]
 ]`);
